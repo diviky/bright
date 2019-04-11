cf7534a1da70f35cb627756c150adaca64c7fb20
Sankar Suda <sankar.suda@gmail.com>
2019-04-11 14:57:14 +0530

    Config merge recursive added

---
+++ src/Support/ServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Karla\Support;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+
+    /**
+     * Merge the given configuration with the existing configuration.
+     *
+     * @param  string  $path
+     * @param  string  $key
+     * @return void
+     */
+    protected function mergeConfigRecursive($path, $key)
+    {
+        $config = $this->app['config']->get($key, []);
+
+        $this->app['config']->set($key, array_merge_recursive(require $path, $config));
+    }
+}
