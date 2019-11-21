931c6b52fd396d71d43c6dd993f8ebdf449415f7
Sankar Suda <sankar.suda@gmail.com>
2019-11-21 15:27:29 +0530

    database table prefix fixed

--- src/Http/Middleware/PreflightResponse.php
+++ src/Http/Middleware/PreflightResponse.php
@@ -28,7 +28,7 @@ class PreflightResponse
     public function addCorsHeaders($response)
     {
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', '*');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,Authorization,Origin,Accept,Access-Control-Allow-Headers,Access-Control-Allow-Methods,Access-Control-Allow-Origin,*');
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
 
