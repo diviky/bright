b39c4c19b14a07c2b2fd4eec7d80647ed70dcbb3
Sankar Suda <sankar.suda@gmail.com>
2020-09-18 16:04:42 +0530

    alert changes

--- resources/assets/js/karla/util.js
+++ resources/assets/js/karla/util.js
@@ -1,9 +1,29 @@
 function uuid() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() +
+      s4() + s4();
+}
+
+var box = {
+  confirm: function(message, callback) {
+    if (message == '' || message == undefined) {
+      return callback(true);
     }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
+
+    if (bootbox && bootbox != undefined) {
+      bootbox.confirm({
+        message: message,
+        callback: function(result) {
+          callback(result);
+        }
+      });
+    } else {
+      if (!confirm(message)) {
+        return callback(false);
+      }
+      callback(true);
+    }
+  }
 }
