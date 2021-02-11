00084c015d18f3cdcc0c1ea5b5c303e0112606f0
Sankar Suda <sankar.suda@gmail.com>
2021-02-11 15:05:43 +0530

    registration changes

--- src/Http/Controllers/Auth/Concerns/RegistersUsers.php
+++ src/Http/Controllers/Auth/Concerns/RegistersUsers.php
@@ -2,20 +2,64 @@
 
 namespace Diviky\Bright\Http\Controllers\Auth\Concerns;
 
-use Diviky\Bright\Models\Models;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Diviky\Bright\Models\Models;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Validator;
+use Diviky\Bright\Notifications\SendActivationToken;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 trait RegistersUsers
 {
-    public function register($values)
+    public function registers(array $values)
     {
         event(new Registered($user = $this->create($values)));
 
-        return $this->registered($user);
+        if ($response = $this->registered($user)) {
+            return $response;
+        }
     }
 
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $values = $request->all();
+        $this->validator($values)->validate();
+
+        if ($response = $this->registers($values)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+                    ? new JsonResponse([], 201)
+                    : redirect($this->redirectPath());
+    }
+
+    /**
+     * Get the guard to be used during registration.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+    /**
+     * The user has been registered.
+     *
+     * @param mixed $user
+     *
+     * @return mixed
+     */
     protected function registered($user)
     {
         //Assign a role to user
@@ -28,7 +72,34 @@ trait RegistersUsers
 
         $user->assignParent();
 
-        return $user;
+        if (0 == $user->status) {
+            $token = $this->saveToken($user);
+            $user->notify(new SendActivationToken($token));
+        } else {
+            $this->guard()->login($user);
+        }
+
+        $next = (0 == $user->status) ? 'user.activate' : $this->redirectPath();
+
+        return [
+            'redirect' => $next,
+            'status'   => 'OK',
+            'message'  => \_('Registration success. Redirecting..'),
+        ];
+    }
+
+      /**
+     * Get a validator for an incoming registration request.
+     *
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name'     => 'required|string|max:100',
+            'email'    => 'required|string|email|unique:' . config('bright.table.users'),
+            'password' => 'required|case_diff|numbers|letters|min:6|max:20',
+        ]);
     }
 
     /**
@@ -45,7 +116,21 @@ trait RegistersUsers
             'email'        => $values['email'],
             'password'     => Hash::make($values['password']),
             'status'       => $status,
-            'access_token' => Str::random(36),
+            'access_token' => Str::random(60),
         ]);
+    }
+
+    /**
+     * Get the post register / login redirect path.
+     *
+     * @return string
+     */
+    public function redirectPath()
+    {
+        if (method_exists($this, 'redirectTo')) {
+            return $this->redirectTo();
+        }
+
+        return property_exists($this, 'redirectTo') ? $this->redirectTo : '/';
     }
 }
--- src/Http/Controllers/Auth/RegisterController.php
+++ src/Http/Controllers/Auth/RegisterController.php
@@ -2,18 +2,11 @@
 
 namespace Diviky\Bright\Http\Controllers\Auth;
 
-use App\User;
-use Diviky\Bright\Http\Controllers\Auth\Traits\ColumnsTrait;
-use Diviky\Bright\Http\Controllers\Auth\Traits\Token;
-use Diviky\Bright\Models\Models;
-use Diviky\Bright\Notifications\SendActivationToken;
 use Diviky\Bright\Routing\Controller;
-use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Http\Request;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Str;
+use Diviky\Bright\Http\Controllers\Auth\Traits\Token;
+use Diviky\Bright\Http\Controllers\Auth\Traits\ColumnsTrait;
+use Diviky\Bright\Http\Controllers\Auth\Concerns\RegistersUsers;
 
 class RegisterController extends Controller
 {
@@ -50,69 +43,5 @@ class RegisterController extends Controller
     public function showRegistrationForm()
     {
         return view('bright::auth.register');
-    }
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name'     => 'required|string|max:100',
-            'email'    => 'required|string|email|unique:' . config('bright.table.users'),
-            'password' => 'required|case_diff|numbers|letters|min:6|max:20',
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @return \App\Models\User
-     */
-    protected function create(array $values)
-    {
-        $status = $values['status'] ?? config('auth.user.status', 0);
-
-        return Models::user()::create([
-            'name'         => $values['name'],
-            'email'        => $values['email'],
-            'password'     => Hash::make($values['password']),
-            'status'       => $status,
-            'access_token' => Str::random(60),
-        ]);
-    }
-
-    /**
-     * The user has been registered.
-     *
-     * @param mixed $user
-     *
-     * @return mixed
-     */
-    protected function registered(Request $request, $user)
-    {
-        //Assign a role to user
-        $role = $this->role ?: config('auth.user.role');
-
-        if ($role) {
-            $user->assignRole($role);
-            $user->assignOwnRole($role);
-        }
-        $user->assignParent();
-
-        if (0 == $user->status) {
-            $token = $this->saveToken($user);
-            $user->notify(new SendActivationToken($token));
-        }
-
-        $next = (0 == $user->status) ? 'user.activate' : $this->redirectPath();
-
-        return [
-            'redirect' => $next,
-            'status'   => 'OK',
-            'message'  => \_('Registration success. Redirecting..'),
-        ];
     }
 }
