78c097502080357bd0ccc14c965224fdeb3ff25d
sankar <sankar@mobtexting.com>
2024-04-08 14:07:51 +0300

    feat: increase default timeout for pjax requests

--- resources/assets/js/bootstrap/util.js
+++ resources/assets/js/bootstrap/util.js
@@ -1,63 +1,59 @@
 String.prototype.replaceAll = function (search, replacement) {
     var target = this;
-    return target.replace(new RegExp(search, "g"), replacement);
+    return target.replace(new RegExp(search, 'g'), replacement);
 };
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    var expires = 'expires=' + d.toUTCString();
+    var secure = (location.protocol === 'https:');
+    document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/;SameSite=Lax'+(secure ? ';secure' : '');
 }
 
 function getCookie(cname) {
-    var name = cname + "=";
+    var name = cname + '=';
     var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(";");
+    var ca = decodedCookie.split(';');
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == " ") {
+        while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
             return c.substring(name.length, c.length);
         }
     }
-    return "";
+    return '';
 }
 
 function removeCookie(cname) {
-    document.cookie =
-        cname + "=; expires = Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    document.cookie = cname + '=; expires = Thu, 01 Jan 1970 00:00:00 GMT;path=/';
 }
 
 function nl2br(str, is_xhtml) {
-    if (typeof str === "undefined" || str === null) {
-        return "";
+    if (typeof str === 'undefined' || str === null) {
+        return '';
     }
-    var breakTag =
-        is_xhtml || typeof is_xhtml === "undefined" ? "<br />" : "<br>";
-    return (str + "").replace(
-        /([^>\r\n]?)(\r\n|\n\r|\r|\n)/g,
-        "$1" + breakTag + "$2"
-    );
+    var breakTag = is_xhtml || typeof is_xhtml === 'undefined' ? '<br />' : '<br>';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
 }
 
 // load JSON files from url
 function loadJSON(url, callback, headers) {
     $.ajax({
         url: url,
-        method: "GET",
-        dataType: "json",
+        method: 'GET',
+        dataType: 'json',
         headers: headers,
         success: function (res) {
             callback(res);
         },
         fail: function () {
-            callback(null, "error");
+            callback(null, 'error');
         },
         error: function () {
-            callback(null, "error");
+            callback(null, 'error');
         },
     });
 }
@@ -68,27 +64,14 @@ function uuid() {
             .toString(16)
             .substring(1);
     }
-    return (
-        s4() +
-        s4() +
-        "-" +
-        s4() +
-        "-" +
-        s4() +
-        "-" +
-        s4() +
-        "-" +
-        s4() +
-        s4() +
-        s4()
-    );
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
 const loadDynamicScript = (url, id, callback) => {
-    const existingScript = document.getElementById("scriptId");
+    const existingScript = document.getElementById('scriptId');
 
     if (!existingScript) {
-        const script = document.createElement("script");
+        const script = document.createElement('script');
         script.src = url; // URL for the third-party library being loaded.
         script.id = id; // e.g., googleMaps or stripe
         document.body.appendChild(script);
@@ -107,35 +90,22 @@ function guid() {
             .toString(16)
             .substring(1);
     }
-    return (
-        s4() +
-        s4() +
-        "-" +
-        s4() +
-        "-" +
-        s4() +
-        "-" +
-        s4() +
-        "-" +
-        s4() +
-        s4() +
-        s4()
-    );
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
 jQuery.fn.preventDoubleClick = function () {
-    $(this).on("click", function (e) {
+    $(this).on('click', function (e) {
         var $el = $(this);
-        if ($el.data("clicked")) {
+        if ($el.data('clicked')) {
             // Previously clicked, stop actions
             e.preventDefault();
             e.stopPropagation();
         } else {
             // Mark to ignore next click
-            $el.data("clicked", true);
+            $el.data('clicked', true);
             // Unmark after 1 second
             window.setTimeout(function () {
-                $el.removeData("clicked");
+                $el.removeData('clicked');
             }, 1000);
         }
     });
@@ -143,5 +113,5 @@ jQuery.fn.preventDoubleClick = function () {
 };
 
 RegExp.escape = function (s) {
-    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 };
